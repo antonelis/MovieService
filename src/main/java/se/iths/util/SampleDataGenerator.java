d2df9064f0766f266c01038c26674028253fb3fd
Anton Johansson <anton.elis@hotmail.com>
2020-11-13 16:52:09 +0100

    Implemented DataGenerator

---
+++ src/main/java/se/iths/util/SampleDataGenerator.java
@@ -0,0 +1,27 @@
+package se.iths.util;
+import se.iths.entity.Movie;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Singleton
+@Startup
+public class SampleDataGenerator {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @PostConstruct
+    public void generateData() {
+        entityManager.persist(new Movie("The Fast and the Furious", "Action", "160 min", 2001));
+        entityManager.persist(new Movie("Fight Club", "Drama",  "139 min", 1999));
+        entityManager.persist(new Movie("Pulp Fiction", "Crime", "154 min", 1994));
+        entityManager.persist(new Movie("Matrix", "Sci-Fi", "136 min", 1999));
+        entityManager.persist(new Movie("Parasit", "Drama", "132 min", 2019));
+    }
+
+
+}
